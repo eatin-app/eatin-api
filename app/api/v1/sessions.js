1977d7a99a471a14bb88ecdf6d50674b4e85f8c5
Andreas Hultgren <me@andreashultgren.se>
2014-10-19 14:04:38 +0200

    Verify email if token is correct when fetching session

--- app/api/v1/sessions.js
+++ app/api/v1/sessions.js
@@ -5,7 +5,9 @@ var express = require('express');
 var async = require('async');
 var mongoose = require('mongoose');
 var User = mongoose.model('User');
+var auth = require('../../utils/auth');
 var sessions = express.Router();
+
 
 module.exports = sessions;
 
@@ -13,15 +15,16 @@ sessions.route('')
 .post(passport.authenticate('local', { session: false }), function (req, res) {
   res.json(req.user);
 })
-.get(function (req, res) {
-  res.send(req.user);
-})
-.delete(function (req, res) {
-  if(!req.user) {
-    //## Replace this with general req.isLoggedIn() or something
-    return res.json(401, {});
+.get(auth.isLoggedIn, function (req, res) {
+  if(!req.user.verified) {
+    return req.user.set('verified', true).save(function () {
+      res.send(req.user);
+    });
   }
 
+  res.send(req.user);
+})
+.delete(auth.isLoggedIn, function (req, res) {
   req.user.deleteToken(function (err) {
     res.json(err || { success: true });
   });
